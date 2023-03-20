5c9ec2bfc2ded1360f5947c94443eb002b83e6ba
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-20 21:16:33 +0400

    Add a simple form and bind the input/output

--- src/app.js
+++ src/app.js
@@ -4,43 +4,82 @@ console.log(ReactDOM)
 const app = {
     tittle: 'Indecision App',
     subtitle: 'First react app',
-    options: ['one', 'two']
+    options: []
 }
 
-const template = (
-    <div>
-        <h1 id={'template-id'}>{app.tittle}</h1>
-        {app.subtitle && <h2>{app.subtitle}</h2>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>)
-
-const user = {
-    name: 'Pavel',
-    age: 27,
-    country: 'Georgia'
-}
-
-function getCountry(country) {
-    if (country) {
-        return <p>Country: {country}</p>
+const onAddOption = e => {
+    e.preventDefault()
+    const option = e.target.elements['option']
+    console.log('Form submitted', option.value)
+    if (option) {
+        app.options.push(option.value)
+        option.value = ''
+        renderIndecisionApp()
     }
+    console.log('Available options:', app.options)
 }
 
-function getAge(age) {
-    return (age && age >= 18) && <p>Age: {age}</p>
+const onRemoveAll = () => {
+    app.options = []
+    renderIndecisionApp()
 }
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name.toUpperCase(): 'Anonymous'}</h1>
-        {getAge(user.age)}
-        {getCountry(user.country)}
-    </div>
-)
+let counter = 0
+
+const increment = () => {
+    counter++
+    console.log("Increment")
+    renderCounterApp()
+};
+
+const decrement = () => {
+    counter--
+    console.log("Decrement")
+    renderCounterApp()
+};
+
+const reset = () => {
+    counter = 0
+    console.log("Reset")
+    renderCounterApp()
+};
+
 
 const appRoot = document.getElementById('app')
-ReactDOM.render(template, appRoot)
+
+function renderIndecisionApp() {
+    const template = (
+        <div>
+            <h1 id={'template-id'}>{app.tittle}</h1>
+            {app.subtitle && <h2>{app.subtitle}</h2>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onAddOption}>
+                <input type='text' name='option'/>
+                <button>Add</button>
+            </form>
+        </div>
+    )
+
+    ReactDOM.render(template, appRoot)
+}
+
+function renderCounterApp() {
+    const templateTwo = (
+        <div>
+            <h1>Count: {counter}</h1>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+
+    ReactDOM.render(templateTwo, appRoot)
+}
+
+renderIndecisionApp()
