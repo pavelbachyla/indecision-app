8569b74f64e6ef8a164fef921ef4df11920a0991
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-20 15:28:03 +0400

    Apply basic JSX conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 console.log(React)
 console.log(ReactDOM)
 
+let app = {
+    tittle: 'Indecision App',
+    subtitle: 'First react app',
+    options: ['one', 'two']
+}
+
 let template = (
     <div>
-        <h1 id={'template-id'}>Indecision App</h1>
-        <p>Paragraph</p>
+        <h1 id={'template-id'}>{app.tittle}</h1>
+        {app.subtitle && <h2>{app.subtitle}</h2>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -16,13 +23,24 @@ let user = {
     age: 27,
     country: 'Georgia'
 }
+
+function getCountry(country) {
+    if (country) {
+        return <p>Country: {country}</p>
+    }
+}
+
+function getAge(age) {
+    return (age && age >= 18) && <p>Age: {age}</p>
+}
+
 let templateTwo = (
     <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.country}</p>
+        <h1>{user.name ? user.name.toUpperCase(): 'Anonymous'}</h1>
+        {getAge(user.age)}
+        {getCountry(user.country)}
     </div>
 )
 
 let appRoot = document.getElementById('app')
-ReactDOM.render(templateTwo, appRoot)
+ReactDOM.render(template, appRoot)
