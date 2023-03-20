ddaef6a4bdb76e941eafef66927809ef25df7ea3
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-20 14:33:36 +0400

    Apply placeholders to the template

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 console.log(React)
 console.log(ReactDOM)
 
-let template = <p id={'template-id'}>The React template content. Change detector</p>
+let template = (
+    <div>
+        <h1 id={'template-id'}>Indecision App</h1>
+        <p>Paragraph</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>)
+
+let name = 'Pavel';
+let age = 27;
+let country = 'Georgia';
+let templateTwo = (
+    <div>
+        <h1>{name}</h1>
+        <p>Age: {age}</p>
+        <p>Location: {country}</p>
+    </div>
+)
+
 let appRoot = document.getElementById('app')
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
