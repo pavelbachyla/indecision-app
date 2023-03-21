6da286d7b3e26a36b8d3a802f4dbdfb33fa166c7
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-21 15:20:25 +0400

    Migrate Visibility App to components w state

--- src/playground/visibility.js
+++ src/playground/visibility.js
@@ -1,20 +1,31 @@
 const appRoot = document.getElementById('app')
 
-let visible = false
-function onVisibilityChanged() {
-    visible = !visible
-    renderVisibilityApp()
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleVisibility = this.toggleVisibility.bind(this)
+        this.state = {
+            visible: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Visibility App</h1>
+                <button onClick={this.toggleVisibility}>{this.state.visible ? 'Hide Details' : 'Show Details'}</button>
+                {this.state.visible && <p>Hey! There are some details you can now see</p>}
+            </div>
+        )
+    }
+
+    toggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visible: !prevState.visible
+            }
+        })
+    }
 }
 
-function renderVisibilityApp() {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={onVisibilityChanged}>{visible ? 'Hide Details' : 'Show Details'}</button>
-            {visible && <p>Hey! There are some details you can now see</p>}
-        </div>
-    )
-    ReactDOM.render(template, appRoot)
-}
-
-renderVisibilityApp()
+ReactDOM.render(<VisibilityToggle />, appRoot)
