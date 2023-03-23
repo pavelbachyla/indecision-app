9b9008cb6632e4a2e2f6695055b4c8bb4b755794
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-23 15:24:11 +0400

    Simplify stateless components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.option}
-            <button onClick={(e) => {
-                props.handleDeleteOption(props.option)
-            }}>remove
-            </button>
-        </div>
-    )
-}
+const Option = (props) => (
+    <div>
+        {props.option}
+        <button onClick={(e) => {
+            props.handleDeleteOption(props.option)
+        }}>remove
+        </button>
+    </div>
+)
 
 export {Option}
