90f07010d1d6a66241d758c8999ff0cc77430b9b
Pavel Bachyla <Pavel.Bachyla@gmail.com>
2023-03-23 19:55:03 +0400

    Apply styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Header = (props) => (
-    <div>
-        <h1 id={'template-id'}>{props.title}</h1>
-        {props.subtitle && <h2>{props.subtitle}</h2>}
+    <div className='header'>
+        <h1 className='header__tittle'>{props.title}</h1>
+        {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
     </div>
 )
 
